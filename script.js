996d5908b0fc2dc392ea44087ca3f76cc78e66ab
BooogyTheGreat <145370226+BooogyTheGreat@users.noreply.github.com>
2023-11-20 09:46:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("num1").addEventListener("click", function1);
+document.getElementById("num2").addEventListener("click", function2);
+document.getElementById("num3").addEventListener("click", function3);
+document.getElementById("num4").addEventListener("click", function4);
+document.getElementById("num5").addEventListener("click", function5);
+
+function function1() {
+  alert("Time");
+}
+function function2() {
+  alert("Night");
+}
+function function3() {
+  alert("Bees");
+}
+function function4() {
+  alert("Great responsibility");
+}
+function function5() {
+  alert("Thin");
+}
